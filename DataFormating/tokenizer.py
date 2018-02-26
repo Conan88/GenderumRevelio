d713d9bf688d43d5d10ef67188d3d654e8ddab32
Christoffer Berglund <christoffer.berglund.privat@gmail.com>
2018-02-26 10:31:02 +0100

    Testing stuff

--- DataFormating/tokenizer.py
+++ DataFormating/tokenizer.py
@@ -3,6 +3,7 @@
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.text import text_to_word_sequence
 from keras.preprocessing.text import hashing_trick
+import numpy as np
 
 
 t = Tokenizer()
@@ -13,4 +14,9 @@ encoded_data = []
 with open('../Data/test.txt', 'r', encoding='latin-1') as f:
     for line in f:
         data = ttt(line, 100)
-        print(data)
+        encoded_data.append(data)
+
+for i in encoded_data:
+    print(i)
+
+# np.savetxt('test.out', encoded_data)
