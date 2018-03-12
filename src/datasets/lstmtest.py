c4ae73625b5ce2854812f22ed7098eec08680a03
James Giang <kjgian15@student.uia.no>
2018-03-12 19:30:03 +0100

    Save results for each network run

--- src/datasets/lstmtest.py
+++ src/datasets/lstmtest.py
@@ -19,13 +19,15 @@ from keras.models import Sequential
 from keras.layers import Dense, Embedding
 from keras.layers import LSTM, Bidirectional
 from postdata import load_data
-import pickle
-import numpy
-
+import shutil
+import json
+import os
+import time
+import datetime
 
 max_features = 20000
 maxlen = 80  # cut texts after this number of words (among top max_features most common words)
-batch_size = 32
+batch_size = 124
 
 print('Loading data...')
 (x_train, y_train), (x_test, y_test) = load_data()
@@ -44,7 +46,7 @@ model.add(Embedding(max_features, 128))
 model.add(Bidirectional(LSTM(128, dropout=0.2, recurrent_dropout=0.2)))
 model.add(Dense(100, activation='relu'))
 model.add(Dense(50, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(2, activation='sigmoid'))
 
 # try using different optimizers and different optimizer configs
 model.compile(loss='binary_crossentropy',
@@ -54,22 +56,38 @@ model.compile(loss='binary_crossentropy',
 print('Train...')
 history_callback = model.fit(x_train, y_train,
           batch_size=batch_size,
-          epochs=15,
+          epochs=50,
           validation_data=(x_test, y_test))
 score, acc = model.evaluate(x_test, y_test,
                             batch_size=batch_size)
 print('Test score:', score)
 print('Test accuracy:', acc)
 
-with open("call.pickle", "wb") as file:
-    pickle.dump(history_callback, file)
+i = 1
+prename = "network"
 
-loss_history = history_callback.history["loss"]
-print("-------")
-print(loss_history)
-print("-------")
-print(history_callback)
+while os.path.isdir(prename + str(i)):
+    i += 1
+path = prename + str(i)
+os.mkdir(path)
 
+# Save data on loss and accuracy for each epoch
+with open(os.path.join(path, "callback.json"), "w") as file:
+    j = json.dumps(history_callback.history)
+    file.write(j)
+print(history_callback.history)
+print("Saved callback history")
 
-numpy_loss_history = numpy.array(loss_history)
-numpy.savetxt("loss_history3.txt", numpy_loss_history, delimiter=",")
+# Save the actual file/setting (i.e. a copy of this script)
+shutil.copy(__file__, os.path.join(path, os.path.basename(__file__)))
+"Copied file"
+
+# Save the test accuracy and test score
+with open(os.path.join(path, "results.txt"), "w") as file:
+    file.write(st=datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+    file.write('Test score: ' + str(score) + "\n")
+    file.write('Test accuracy: ' + str(acc) + "\n")
+print("Saved test results")
+
+model.save(os.path.join(path, "my_model.h5"))
+print("Saved model")
